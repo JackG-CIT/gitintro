bc7fc615cc0dad402f25c809d1f370ba8f9c19a9
Zachary Lin (hcps-linzc) <hcps-linzc@henricostudents.org>
2025-09-05 13:49:31 -0400

    added let it grow

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Hello World!")
 print("New Line")
 print("Lemme make a new Branchy branch")
+print("Let it grow let it grow let the love inside you show plant a seed inside the earth just one way to know its worth we'll celebrate the world's rebirth we say let it grow")
